3a0617cce8d546e064d21a0d913ba54acb0de5bd
Tiago Correia <tiko165@gmail.com>
2020-05-24 19:00:52 +0100

    Melhorias finais
    Melhorias finais

--- js/scene/Instrucoes.js
+++ js/scene/Instrucoes.js
@@ -12,12 +12,16 @@ export default class instrucoes extends Phaser.Scene{
 
         this.labelName = this.add.text(width-450, height-600, "Teclas Jogador 1:", {
             font: "12px magv5",
-            fill: "#ff0000"
+            fill: "#ff0000",
+            stroke: "#ffff00 ",
+            strokeThickness: 2
         });
 
         this.labelName = this.add.text(width-400, height-580, "Esquerda: ", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
 
         this.img = this.add.image(width-80,height-570,"se");
@@ -25,7 +29,9 @@ export default class instrucoes extends Phaser.Scene{
 
         this.labelName = this.add.text(width-400, height-560, "Direita:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
 
         this.img = this.add.image(width-80,height-550,"sd");
@@ -33,7 +39,9 @@ export default class instrucoes extends Phaser.Scene{
 
         this.labelName = this.add.text(width-400, height-540, "Cima:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
 
         this.img = this.add.image(width-80,height-530,"sc");
@@ -41,7 +49,9 @@ export default class instrucoes extends Phaser.Scene{
 
         this.labelName = this.add.text(width-400, height-520, "Baixo:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
         
         this.img = this.add.image(width-80,height-510,"sb");
@@ -49,7 +59,9 @@ export default class instrucoes extends Phaser.Scene{
 
         this.labelName = this.add.text(width-400, height-500, "Disparo:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
 
         this.img = this.add.image(width-80,height-490,"sp");
@@ -57,12 +69,16 @@ export default class instrucoes extends Phaser.Scene{
         
         this.labelName = this.add.text(width-450, height-460, "Teclas Jogador 2:", {
             font: "12px magv5",
-            fill: "#ff0000"
+            fill: "#ff0000",
+            stroke: "#ffff00 ",
+            strokeThickness: 2
         });
 
         this.labelName = this.add.text(width-400, height-440, "Esquerda: ", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
 
 
@@ -71,7 +87,9 @@ export default class instrucoes extends Phaser.Scene{
 
         this.labelName = this.add.text(width-400, height-420, "Direita:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
 
         this.img = this.add.image(width-80,height-410,"dd");
@@ -79,7 +97,9 @@ export default class instrucoes extends Phaser.Scene{
 
         this.labelName = this.add.text(width-400, height-400, "Cima:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
 
         this.img = this.add.image(width-80,height-390,"wc");
@@ -87,96 +107,149 @@ export default class instrucoes extends Phaser.Scene{
 
         this.labelName = this.add.text(width-400, height-380, "Baixo:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
         this.img = this.add.image(width-80,height-370,"ss");
         this.img.setScale(0.2);     
         this.labelName = this.add.text(width-400, height-360, "Disparo:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
         this.img = this.add.image(width-80,height-350,"xp");
         this.img.setScale(0.2); 
-        this.labelName = this.add.text(width-450, height-320, "Outras Teclas:", {
+        this.labelName = this.add.text(width-450, height-330, "Outras Teclas:", {
             font: "12px magv5",
-            fill: "#ff0000"
+            fill: "#ff0000",
+            stroke: "#ffff00 ",
+            strokeThickness: 2
         });
 
-        this.labelName = this.add.text(width-400, height-300, "Ligar/Desligar Música: ", {
+        this.labelName = this.add.text(width-400, height-310, "Ligar/Desligar Música: ", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
-        this.img = this.add.image(width-80,height-295,"vq");
+        this.img = this.add.image(width-80,height-305,"vq");
         this.img.setScale(0.2); 
 
-        this.labelName = this.add.text(width-400, height-280, "Baixar/Aumentar Volume Música:", {
+        this.labelName = this.add.text(width-400, height-290, "Baixar/Aumentar Volume Música:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
 
 
-        this.img = this.add.image(width-80,height-275,"vu");
+        this.img = this.add.image(width-80,height-285,"vu");
         this.img.setScale(0.2);
         
-        this.img = this.add.image(width-100,height-275,"vy");
+        this.img = this.add.image(width-100,height-285,"vy");
         this.img.setScale(0.2);
 
-        this.labelName = this.add.text(width-400, height-260, "Pausar Jogo:", {
+        this.labelName = this.add.text(width-400, height-270, "Pausar Jogo:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
-        this.img = this.add.image(width-80,height-255,"ps");
+        this.img = this.add.image(width-80,height-265,"ps");
         this.img.setScale(0.2);
-        this.labelName = this.add.text(width-450, height-220, "Bónus no Jogo:", {
+
+
+
+
+
+        this.labelName = this.add.text(width-450, height-235, "Bónus no Jogo:", {
             font: "12px magv5",
-            fill: "#ff0000"
+            fill: "#ff0000",
+            stroke: "#ffff00 ",
+            strokeThickness: 2
         });
 
 
-        this.img = this.add.image(width-400,height-190,"bonus1");
-        this.img.setScale(0.6);
+        this.img = this.add.image(width-390,height-210,"bonus1");
+        this.img.setScale(0.5);
 
-        this.labelName = this.add.text(width-320, height-195, "Proteção de Escudo", {
+        this.labelName = this.add.text(width-370, height-218, "Proteção de Escudo", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });       
 
-        this.img = this.add.image(width-400,height-160,"bonus2");
-        this.img.setScale(0.6);
+        this.img = this.add.image(width-390,height-185,"bonus2");
+        this.img.setScale(0.5);
 
-        this.labelName = this.add.text(width-320, height-165, "Vida Extra", {
+        this.labelName = this.add.text(width-370, height-192, "Vida Extra", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });    
         
-        this.labelName = this.add.text(width-450, height-130, "Pontuação:", {
+
+
+
+        this.labelName = this.add.text(width-450, height-150, "Pontuação:", {
             font: "12px magv5",
-            fill: "#ff0000"
+            fill: "#ff0000",
+            stroke: "#ffff00 ",
+            strokeThickness: 2
         });
 
-        this.labelName = this.add.text(width-400, height-110, "Nave Inimiga Abatida:", {
+        this.labelName = this.add.text(width-400, height-130, "Nave Inimiga Abatida:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
 
-        this.labelName = this.add.text(width-170, height-110, "Nivel * Vidas * 2", {
+        this.labelName = this.add.text(width-170, height-130, "Nivel * Vidas * 2", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
         
-        this.labelName = this.add.text(width-400, height-90, "Nivel Seguinte:", {
+
+        this.labelName = this.add.text(width-400, height-110, "Nivel Seguinte:", {
             font: "12px magv5",
-            fill: "#ffffff"
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
+        });
+        this.labelName = this.add.text(width-170, height-110, "50 Pontos", {
+            font: "12px magv5",
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
+        });
+        
+
+        this.labelName = this.add.text(width-400, height-90, "Tiros no Boss:", {
+            font: "12px magv5",
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
+        });
+        this.labelName = this.add.text(width-170, height-90, "Nivel do boss", {
+            font: "12px magv5",
+            fill: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 6
         });
 
-        this.labelName = this.add.text(width-170, height-90, "50 Pontos", {
-            font: "12px magv5",
-            fill: "#ffffff"
-        });
 
-        this.labelName = this.add.text(width-380, height-50, "Press SPACE to continue", {
+        this.labelName = this.add.text(width-380, height-40, "Press SPACE to continue", {
             font: "20px magv5",
-            fill: "#ff0000"
+            fill: "#ff0000",
+            stroke: "#000000",
+            strokeThickness: 6
         });
         
 
